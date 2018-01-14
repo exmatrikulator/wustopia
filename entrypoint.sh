de009016e28f837f60d0bf3c9580095b73a25b91
Ex <ex@devtal.de>
2018-01-14 16:45:45 +0100

    allow to install packages in develop mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,20 @@ if [ -z "$@" ]; then
         cp /app/config.py.example /app/config/config.py
     fi
 
+
+    #if app is mounted, Dockerfile is available
+    if [ -f Dockerfile ]; then
+        pip3 install -r requirements.txt
+    fi
+
+    # if /app/migrations is empty init database
     if [ ! -f /app/migrations/README ]; then
         python3 manage.py db init -d /tmp/migrations 2> /dev/null
         mv /tmp/migrations/* /app/migrations
     fi
 
     #wait for postgres
-    until python3 manage.py db current > /dev/null 2>&1 ; do
+    until python3 manage.py db current > /dev/null ; do
       >&2 echo "Postgres is unavailable - sleeping"
       sleep 1
     done
