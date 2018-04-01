b483a3b300e0c6b487646ccf1a0ebfb0f8721995
Ex <ex@devtal.de>
2018-04-01 20:47:12 +0200

    Order balance

--- webapp/finance.py
+++ webapp/finance.py
@@ -8,10 +8,9 @@ from sqlalchemy import and_
 import datetime as datetime
 
 def getBalance(userid):
-    amounts = db.session.query(Balance).options(joinedload(Balance.resource)).filter_by(user=userid).all()
+    amounts = db.session.query(Balance).options(joinedload(Balance.resource)).filter_by(user=userid).order_by(Balance.id).all()
     return amounts
 
 def getBalanceofResource(userid, resource):
     amounts = db.session.query(Balance).options(joinedload(Balance.resource)).filter_by(user=userid, resource_id=resource).first()
     return amounts
-
